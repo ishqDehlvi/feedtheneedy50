0e026dbb69b50e894061c2fcf577d63313a785ee
Inzemam ul-Haq <Inzemamhaq05@gmail.com>
2021-09-12 06:17:11 +0000

    deleted af

--- components/gallery/gallery-testimonials.js
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const GalleryTestimonials = ({ children }) => {
-  return <div className="gallery-testimonials-parallax">{children}</div>;
-};
-
-export default GalleryTestimonials;
